28504f5b5ae202fff8645f9f91606afa64001bbb
Nikola Milivojevic <0dziga0@gmail.com>
2015-03-20 00:06:14 +0100

    remove comment

--- src/main/java/com/github/dziga/orest/api/ORest.java
+++ src/main/java/com/github/dziga/orest/api/ORest.java
@@ -35,9 +35,6 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
-/**
- * Created by n.milivojevic on 3/13/2015.
- */
 public interface ORest {
 
     /**
